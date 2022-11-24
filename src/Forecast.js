10ecb42b224963e4562aaff8b60de3a1962dfd6d
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-11-24 20:23:00 +0100

    Added Forecast component with CSS styling

---
+++ src/Forecast.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Forecast.css";
+
+export default function Forecast() {
+  return (
+    <div className="Forecast">
+      <div className="weather-info2">
+        <div className="weather-forecast"></div>
+      </div>
+    </div>
+  );
+}
