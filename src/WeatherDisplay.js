ee6baa11d87c2a4f5a04744bf25ef85b124dc1b6
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-11-26 22:08:06 +0100

    Added SheCodes weather API

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,92 +1,103 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 import "./WeatherDisplay.css";
 
 export default function WeatherDisplay() {
-  let defaultWeatherDisplay = {
-    sunIcon: <i class="fa-solid fa-sun"></i>,
-    value: 16,
-    city: "Berlin",
-    country: "DE",
-    weather: "Clear sky",
-    date: "Saturday, 21:21",
-    minTemp: 13,
-    maxTemp: 18,
-    precipitation: 0,
-    wind: 2,
-    humidity: 87,
-  };
-  return (
-    <div className="WeatherDisplay">
-      <div className="d-flex row w-100 mt-2 m-0 justify-content-between weather-all">
-        <div className="col-6 my-auto p-0 sun-info">
-          <div className="cityWeather">
-            <div className="city">
-              <h1>
-                {defaultWeatherDisplay.city},{" "}
-                <span className="country">{defaultWeatherDisplay.country}</span>
-              </h1>
-              <p className="mb-2 date">
-                Last updated: <span>{defaultWeatherDisplay.date}</span>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function displayWeather(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      date: `Saturday, 20:40`,
+      city: response.data.city,
+      country: response.data.country,
+      humidity: response.data.temperature.humidity,
+      minTemp: 13,
+      maxTemp: 18,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="WeatherDisplay">
+        <div className="d-flex row w-100 mt-2 m-0 justify-content-between weather-all">
+          <div className="col-6 my-auto p-0 sun-info">
+            <div className="cityWeather">
+              <div className="city">
+                <h1>
+                  {weatherData.city},{" "}
+                  <span className="country">{weatherData.country}</span>
+                </h1>
+                <p className="mb-2 date">
+                  Last updated: <span>{weatherData.date}</span>
+                  <br />
+                  <span className="text-capitalize">
+                    {weatherData.description}
+                  </span>
+                </p>
+              </div>
+              <div>
+                <p className="iconTemp">
+                  {Math.round(weatherData.temperature)}
+                  <span className="degree">
+                    <sup>°C</sup>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 px-0 text-center my-auto">
+            <ReactAnimatedWeather
+              icon={`CLEAR_DAY`}
+              color={`#e4f5a3`}
+              size={125}
+              animate={true}
+            />
+          </div>
+        </div>
+        <div className="container mt-4 weather-info">
+          <div className="d-flex row w-100 py-2 text-center justify-content-between min-max-temp">
+            <div className="col-3">
+              <p>
+                Min. temperature:
                 <br />
-                {defaultWeatherDisplay.weather}
+                <span>{weatherData.minTemp}</span>°C
               </p>
             </div>
-            <div>
-              <p className="iconTemp">
-                {defaultWeatherDisplay.value}
-                <span className="degree">
-                  <sup>°C</sup>
-                </span>
+            <div className="col-3 border-start">
+              <p>
+                Max. temperature:
+                <br />
+                <span>{weatherData.maxTemp}</span>°C
               </p>
             </div>
-          </div>
-        </div>
-        <div className="col-6 px-0 text-center my-auto">
-          <ReactAnimatedWeather
-            icon={`CLEAR_DAY`}
-            color={`#e4f5a3`}
-            size={125}
-            animate={true}
-          />
-        </div>
-      </div>
-      <div className="container mt-4 weather-info">
-        <div className="d-flex row w-100 py-2 text-center justify-content-between min-max-temp">
-          <div className="col-3 ">
-            <p>
-              Min:
-              <span className="min-temp">13</span>
-              °C
-              <br />
-              Max: <span className="max-temp">18</span>
-              °C
-            </p>
-          </div>
-          <div className="col-3 border-start">
-            <p>
-              Precipitation:
-              <br />
-              <span>0</span> mm
-            </p>
-          </div>
-          <div className="col-3 border-start">
-            <p>
-              Wind speed:
-              <br />
-              <span>2</span> km/h
-            </p>
-          </div>
-          <div className="col-3 border-start">
-            <p>
-              Humidity:
-              <br />
-              <span>87</span> %
-            </p>
+            <div className="col-3 border-start">
+              <p>
+                Wind speed:
+                <br />
+                <span>{Math.round(weatherData.wind)}</span> km/h
+              </p>
+            </div>
+            <div className="col-3 border-start">
+              <p>
+                Humidity:
+                <br />
+                <span>{weatherData.humidity}</span> %
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `3t89a3048deco3ffc8f06ed499bd33b4`;
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayWeather);
+  }
 }
