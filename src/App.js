a827081c585288bc0827ee435369a5e00a9b6547
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-11-29 23:43:31 +0100

    Removed the quote of the day & improved the footer links

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ export default function App() {
     <div className="App">
       <div className="container p-4 container-all">
         <Search defaultCity="Berlin" />
-        <div className="container">
-          <div className="quote">
-            <p className="quote-of-day">
-              "Keep your eyes lifted high upon the sun, and you'll see the best
-              light in everyone."
-            </p>
-          </div>
-        </div>
+        <div className="container"></div>
       </div>
       <footer>
         <p>
-          <a href="https://github.com/meytiri1/my-weather-app-react">
+          <a
+            href="https://github.com/meytiri1/my-weather-app-react"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
             Open-source code
           </a>{" "}
-          by Michelle Köhler, hosted via Netlify
+          by{" "}
+          <a
+            href="https://bright-centaur-7034e8.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Michelle Köhler
+          </a>
+          , hosted via Netlify
         </p>
       </footer>
     </div>
