cf9a2403e63d84121814e0427c00db082fc8f97d
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-11-30 01:05:10 +0100

    Removed current location button & save its code to a text file

--- src/Search.js
+++ src/Search.js
@@ -47,29 +47,19 @@ export default function Search(props) {
     return (
       <div className="Search">
         <form className="city-search-form" onSubmit={handleSubmit}>
-          <div className="d-flex">
-            <div className="col-6 mb-2">
-              <div className="input-group rounded city-search">
-                <input
-                  type="search"
-                  className="form-control"
-                  placeholder="Search city"
-                  aria-label="Search"
-                  aria-describedby="search-addon"
-                  onChange={updateCity}
-                />
-                <span className="input-group-text border-start-0 rounded-start search-btn">
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </span>
-              </div>
-            </div>
-            <div className="col-6 ms-4">
-              <button
-                className="btn px-3 py-2 align-items-center location-btn"
-                type="button"
-              >
-                <i className="fa-solid fa-location-dot"></i>
-              </button>
+          <div className="col-6 mb-2">
+            <div className="input-group rounded city-search">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Search city"
+                aria-label="Search"
+                aria-describedby="search-addon"
+                onChange={updateCity}
+              />
+              <span className="input-group-text border-start-0 rounded-start search-btn">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </span>
             </div>
           </div>
         </form>
